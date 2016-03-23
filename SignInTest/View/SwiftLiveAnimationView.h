3b52aaf886cd7a2a4f56f0b8b4c185f6f541b14b
VictorQi <hust_yjc@163.com>
2016-03-23 18:08:45 +0800

    add animation view

---
+++ SignInTest/View/SwiftLiveAnimationView.h
@@ -0,0 +1,13 @@
+//
+//  SwiftLiveAnimationView.h
+//  SignInTest
+//
+//  Created by Victor on 16/3/23.
+//  Copyright © 2016年 DotC_United. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwiftLiveAnimationView : UIView
+
+@end
---
+++ SignInTest/View/SwiftLiveAnimationView.m
@@ -0,0 +1,21 @@
+//
+//  SwiftLiveAnimationView.m
+//  SignInTest
+//
+//  Created by Victor on 16/3/23.
+//  Copyright © 2016年 DotC_United. All rights reserved.
+//
+
+#import "SwiftLiveAnimationView.h"
+
+@implementation SwiftLiveAnimationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
